6083d0d4b4848f0d840abe6b675b1446978f22a8
Luis Angel <Luis Angel@LUIS>
2023-03-26 19:52:09 -0600

    Agregar archivos de proyecto.

---
+++ SalaDeBelleza/Views/MyPerfil.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SalaDeBelleza.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyPerfil : ContentPage
+    {
+        public MyPerfil()
+        {
+            InitializeComponent();
+        }
+
+        private void btnagregarimagen_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncerrar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SalaDeBelleza/Views/Vperfil.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SalaDeBelleza.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Vperfil : ContentPage
+    {
+        public Vperfil()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFoto_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnActualizarDatos_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnActualizarClave_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCerrarSesion_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminarCuenta_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
